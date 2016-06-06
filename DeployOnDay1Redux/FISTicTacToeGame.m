2102ae9f99e8d166a86b7eb9e55934d69785e5d8
kenpcjr <kennethcooke@me.com>
2016-06-06 12:56:49 -0400

    First Pass, All Methods Work

--- DeployOnDay1Redux/FISTicTacToeGame.m
+++ DeployOnDay1Redux/FISTicTacToeGame.m
@@ -21,9 +21,11 @@
 {
     self = [super init];
     if(self) {
-        // Do initialization of your game here, inside this if statement.
-        // Leave the rest of this method alone :)
-
+        
+        _board =[@[
+                      [@[@"",@"",@""]mutableCopy],
+                      [@[@"",@"",@""]mutableCopy],
+                      [@[@"",@"",@""]mutableCopy]]mutableCopy];
     }
 
     return self;
@@ -31,37 +33,114 @@
 
 -(void)resetBoard
 {
+    
+    self.board =[@[
+                  [@[@"",@"",@""]mutableCopy],
+                  [@[@"",@"",@""]mutableCopy],
+                  [@[@"",@"",@""]mutableCopy]]mutableCopy];
 
 }
 
 -(NSString *)playerAtColumn:(NSUInteger)column row:(NSUInteger)row
 {
-    return @"";
+    NSString *playerPosition = @"";
+    playerPosition = self.board[column][row];
+    
+    
+    return playerPosition;
+    NSLog(@"%@", playerPosition);
 }
 
 -(BOOL)canPlayAtColumn:(NSUInteger)column row:(NSUInteger)row
 {
-    return YES;
+    if ([self.board[column][row]isEqualToString:@""]) {
+        return YES;
+    }
+    return NO;
 }
 
 -(void)playXAtColumn:(NSUInteger)column row:(NSUInteger)row
 {
+    self.board[column][row]=@"X";
 
 }
 
 -(void)playOAtColumn:(NSUInteger)column row:(NSUInteger)row
 {
+    self.board[column][row]=@"O";
 
 }
 
 -(NSString *)winningPlayer
 {
+    if ([self.board[0] isEqual:@[@"X",@"X",@"X"]]) {
+        return @"X";
+    }
+    if ([self.board[1] isEqual:@[@"X",@"X",@"X"]]) {
+        return @"X";
+    }
+    if ([self.board[2] isEqual:@[@"X",@"X",@"X"]]) {
+        return @"X";
+    }
+    NSString *xColumn0 = [NSString stringWithFormat:@"%@,%@,%@",self.board[0][0],self.board[1][0],self.board[2][0]];
+    if ([xColumn0 isEqualToString:@"X,X,X"]) {
+        return @"X";
+    }
+    NSString *xColumn1 = [NSString stringWithFormat:@"%@,%@,%@",self.board[0][1],self.board[1][1],self.board[2][1]];
+    if ([xColumn1 isEqualToString:@"X,X,X"]) {
+        return @"X";
+    }
+    NSString *xColumn2 = [NSString stringWithFormat:@"%@,%@,%@",self.board[0][2],self.board[1][2],self.board[2][2]];
+    if ([xColumn2 isEqualToString:@"X,X,X"]) {
+        return @"X";
+    }
+    NSString *xDiagonal0 = [NSString stringWithFormat:@"%@,%@,%@",self.board[0][0],self.board[1][1],self.board[2][2]];
+    if ([xDiagonal0 isEqualToString:@"X,X,X"]) {
+        return @"X";
+    }
+    NSString *xDiagonal2 = [NSString stringWithFormat:@"%@,%@,%@",self.board[0][2],self.board[1][1],self.board[2][0]];
+    if ([xDiagonal2 isEqualToString:@"X,X,X"]) {
+        return @"X";
+    }
+    
+    if ([self.board[0] isEqual:@[@"O",@"O",@"O"]]) {
+        return @"O";
+    }
+    if ([self.board[1] isEqual:@[@"O",@"O",@"O"]]) {
+        return @"O";
+    }
+    if ([self.board[2] isEqual:@[@"O",@"O",@"O"]]) {
+        return @"O";
+    }
+    NSString *oColumn0 = [NSString stringWithFormat:@"%@,%@,%@",self.board[0][0],self.board[1][0],self.board[2][0]];
+    if ([oColumn0 isEqualToString:@"O,O,O"]) {
+        return @"O";
+    }
+    NSString *oColumn1 = [NSString stringWithFormat:@"%@,%@,%@",self.board[0][1],self.board[1][1],self.board[2][1]];
+    if ([oColumn1 isEqualToString:@"O,O,O"]) {
+        return @"O";
+    }
+    NSString *oColumn2 = [NSString stringWithFormat:@"%@,%@,%@",self.board[0][2],self.board[1][2],self.board[2][2]];
+    if ([oColumn2 isEqualToString:@"O,O,O"]) {
+        return @"O";
+    }
+    NSString *oDiagonal0 = [NSString stringWithFormat:@"%@,%@,%@",self.board[0][0],self.board[1][1],self.board[2][2]];
+    if ([oDiagonal0 isEqualToString:@"O,O,O"]) {
+        return @"O";
+    }
+    NSString *oDiagonal2 = [NSString stringWithFormat:@"%@,%@,%@",self.board[0][2],self.board[1][1],self.board[2][0]];
+    if ([oDiagonal2 isEqualToString:@"O,O,O"]) {
+        return @"O";
+    }
     return @"";
 }
 
 -(BOOL)isADraw
 {
-    return NO;
+    if ([self.board[0] containsObject:@""] || [self.board[1] containsObject:@""] || [self.board[2] containsObject:@""] ) {
+        return NO;
+    }
+    return YES;
 }
 
 @end
